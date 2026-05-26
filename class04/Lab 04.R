727287941246346ac9672c85feac4e3623251a05
qimanhu85 <qimanhu85@gmail.com>
2026-05-26 10:53:30 -0700

    add previous class projects

---
+++ class04/Lab 04.R
@@ -0,0 +1,23 @@
+#' ---
+#' title: "Lab 04"
+#' author: "Qiman Hu"
+#' ---
+
+
+# Click the Play button to run this code
+5 + 3
+
+# complete code below so answer is 10
+
+5 + 3 + 2
+
+# make assignment and inspect the object
+
+x <- 3 * 4  
+x
+
+x <- 1:50
+plot(x)
+
+
+plot(x, sin(x), type = "l", col = "red", lwd = 4)
